5c0d415a8cd67b9145ba29088e7c40e9f305aaa0
Shashank <pshashank569@gmail.com>
2023-04-18 21:03:19 +0530

    - fixed sticky header

--- js/main.js
+++ js/main.js
@@ -37,24 +37,25 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 ///////////////////////////////////////
 // Sticky navigation: Intersection Observer API
 
+const heroSection = document.querySelector(".hero");
 const header = document.querySelector(".header");
-const navHeight = nav.getBoundingClientRect().height;
+const headerHeight = header.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
   // console.log(entry);
 
-  if (!entry.isIntersecting) nav.classList.add("sticky");
-  else nav.classList.remove("sticky");
+  if (!entry.isIntersecting) document.body.classList.add("sticky");
+  else document.body.classList.remove("sticky");
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px`,
+  rootMargin: `-${headerHeight}px`,
 });
 
-headerObserver.observe(header);
+headerObserver.observe(heroSection);
 
 ///////////////////////////////////////
 // Reveal sections
